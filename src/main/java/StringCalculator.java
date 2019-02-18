bda6d5bc6aa6def0899705cce875fc8c2e02cce6
kseewer <40600885+kseewer@users.noreply.github.com>
2019-02-18 21:11:20 +0100

    test add = 0

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,28 @@
+
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin.seewer
+ */
+public class StringCalculator {
+
+
+    public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+        return parseInt(text);
+		
+}
+    
+}
+
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin.seewer
+ */
+public class StringCalculatorTest {
+    @Before
+  public void setup(){
+  }
+    
+    @Test
+	public void testEmptyString() {
+            StringCalculator Calculator = new StringCalculator();
+		assertEquals(0, Calculator.add(""));
+}
+    
+}
