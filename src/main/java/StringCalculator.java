a0928747ab459905f89be8c12fd2df78ed3372fe
kseewer <40600885+kseewer@users.noreply.github.com>
2019-02-24 18:36:55 +0100

    V1.0
    Réorganisation du code, ajout de la classe sum()

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,4 @@
     import static java.lang.Integer.parseInt;
-    import java.util.ArrayList;
-    import java.util.List;
-
-    /*
-     * To change this license header, choose License Headers in Project Properties.
-     * To change this template file, choose Tools | Templates
-     * and open the template in the editor.
-     */
 
     /**
      *
@@ -14,45 +6,46 @@
      */
     public class StringCalculator {
 
-        public static int add(String text){
-                    if(text.equals("")){
-                            return 0;
-                    }
-                    else{
-                        String delimiter = ",";
-                        if(text.matches("//(.*)\n(.*)")){
-				delimiter = Character.toString(text.charAt(2));
-				text = text.substring(4);
-                    }
-                        
-                    String numList[] = splitNumbers(text, delimiter +"|\n");
-                        int total = 0;
-                        String negatif = "";
-                        for ( String number : numList){if(parseInt(number) < 0){
-        		if(negatif.equals(""))
-        			negatif = number;
-        		else
-        			negatif += ("," + number);
-}
-                            if(parseInt(number)<1000) {
-                               
-                              total += parseInt(number);
-                            }
-                        }
-                        if(!negatif.equals("")){
-			throw new IllegalArgumentException("Les négatifs ne sont pas acceptés: " + negatif);
-}
-                            return total;
-                    }
-        }
-
-    
-     private static String[] splitNumbers(String numbers, String delimiteur) {
-        return numbers.split(delimiteur);
+     public static int add(String text) {
+      if (text.equals("")) {
+       return 0;
+      } else {
+       String delimiter = ",";
+       if (text.matches("//(.*)\n(.*)")) {
+        delimiter = Character.toString(text.charAt(2));
+        text = text.substring(4);
        }
-}
+
+       String numList[] = splitNumbers(text, delimiter + "|\n");
+       return sum(numList);
+      }
+     }
+
+     private static String[] splitNumbers(String numbers, String delimiteur) {
+      return numbers.split(delimiteur);
+     }
 
 
-   
+     private static int sum(String[] numList) {
 
+      int total = 0;
+      String negatif = "";
+      for (String number: numList) {
+       if (parseInt(number) < 0) {
+        if (negatif.equals(""))
+         negatif = number;
+        else
+         negatif += ("," + number);
+       }
+       if (parseInt(number) < 1000) {
 
+        total += parseInt(number);
+       }
+      }
+      if (!negatif.equals("")) {
+       throw new IllegalArgumentException("Les négatifs ne sont pas acceptés: " + negatif);
+      }
+      return total;
+
+     }
+    }
