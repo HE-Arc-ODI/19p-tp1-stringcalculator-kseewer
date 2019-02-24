fe5ba02b0e75e9ce126ed2655d6b18765029d6ef
kseewer <40600885+kseewer@users.noreply.github.com>
2019-02-24 17:36:39 +0100

    suppression des appels inutiles dans le fichier de test

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -16,50 +16,52 @@ import org.junit.Test;
 public class StringCalculatorTest {
     @Before
   public void setup(){
+    
   }
+    StringCalculator Calculator = new StringCalculator();
     
     @Test
 	public void testEmptyString() {
-            StringCalculator Calculator = new StringCalculator();
+            
 		assertEquals(0, Calculator.add(""));
 }
         
     @Test    
      public void test1Valeure() {
-            StringCalculator Calculator = new StringCalculator();
+            
 		assertEquals(2, Calculator.add("2"));
     
 }   
         
     @Test    
         public void test2Valeursavecvirgule() {
-            StringCalculator Calculator = new StringCalculator();
+           
 		assertEquals(5, Calculator.add("2,3"));
     
 }
         @Test    
         public void test3Valeursavecvirgule() {
-            StringCalculator Calculator = new StringCalculator();
+           
 		assertEquals(8, Calculator.add("2,3,3"));
     
 }
         
       @Test
     public void test1Ligne(){
-         StringCalculator Calculator = new StringCalculator();
+        
     	assertEquals(6, Calculator.add("1\n2,3"));
         
     }
   
       @Test
     public void testPlusGrandMille(){
-         StringCalculator Calculator = new StringCalculator();
+      
     	assertEquals(50, Calculator.add("1000,50"));
 }
     
      @Test
     public void testOtherDelimiter(){
-         StringCalculator Calculator = new StringCalculator();
+        
     	assertEquals(3, Calculator.add("//;\n1;2"));
 }
         
