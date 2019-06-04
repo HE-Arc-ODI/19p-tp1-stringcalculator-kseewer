1eed884c58957cc61e5dd7abe17b7af13344317b
kseewer <kevin.seewer@caramail.com>
2019-06-04 15:33:18 +0200

    refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,51 +1,63 @@
-    import static java.lang.Integer.parseInt;
+import static java.lang.Integer.parseInt;
 
-    /**
-     *
-     * @author kevin.seewer
-     */
-    public class StringCalculator {
+/**
+ * @author kevin.seewer
+ */
+public class StringCalculator {
 
-     public static int add(String text) {
-      if (text.equals("")) {
-       return 0;
-      } else {
-       String delimiter = ",";
-       if (text.matches("//(.*)\n(.*)")) {
-        delimiter = Character.toString(text.charAt(2));
-        text = text.substring(4);
-       }
+  public static int add(String text) {
+    if (text.equals("")) {
+      return 0;
+    } else {
 
-       String numList[] = splitNumbers(text, delimiter + "|\n");
-       return sum(numList);
+      String delimiter = ",";
+
+      if (text.startsWith("//[")) {
+        delimiter = text.substring(2, text.indexOf("\n"));
+        //Coding horror
+        delimiter = delimiter.replace("[", "");
+        delimiter = delimiter.replace("]", "|");
+        //TODO: extract method to manage meta character
+        delimiter = delimiter.replace("*", "\\*");
+        delimiter = delimiter.substring(0, delimiter.length() - 1);
+        text = text.substring(text.indexOf("\n") + 1);
+      } else if (text.startsWith("//")) {
+        delimiter = text.substring(2, text.indexOf("\n"));
+        text = text.substring(text.indexOf("\n") + 1);
       }
-     }
 
-     private static String[] splitNumbers(String numbers, String delimiteur) {
-      return numbers.split(delimiteur);
-     }
-
-
-     private static int sum(String[] numList) {
-
-      int total = 0;
-      String negatif = "";
-      for (String number: numList) {
-       if (parseInt(number) < 0) {
-        if (negatif.equals(""))
-         negatif = number;
-        else
-         negatif += ("," + number);
-       }
-       if (parseInt(number) < 1000) {
-
-        total += parseInt(number);
-       }
-      }
-      if (!negatif.equals("")) {
-       throw new IllegalArgumentException("Les négatifs ne sont pas acceptés: " + negatif);
-      }
-      return total;
-
-     }
+      String numList[] = text.split(delimiter);
+      return sum(numList, delimiter);
     }
+  }
+
+  private static String[] splitNumbers(String numbers, String delimiteur) {
+    return numbers.split(delimiteur);
+  }
+
+
+  private static int sum(String[] numList, String delimiter) {
+
+    int total = 0;
+    String negatif = "";
+    for (int i = 0; i < numList.length; i++) {
+
+      if (parseInt(numList[i]) < 0) {
+        if (negatif.equals("")) {
+          negatif = numList[i];
+        } else {
+          negatif += ("," + numList[i]);
+        }
+      }
+      if (parseInt(numList[i]) <= 1000) {
+
+        total += parseInt(numList[i]);
+      }
+    }
+    if (!negatif.equals("")) {
+      throw new IllegalArgumentException("Les négatifs ne sont pas acceptés: " + negatif);
+    }
+    return total;
+
+  }
+}
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,90 +1,86 @@
 
 import static junit.framework.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author kevin.seewer
  */
 public class StringCalculatorTest {
-    @Before
-  public void setup(){
-    
-  }
-    StringCalculator Calculator = new StringCalculator();
-    
-    @Test
-	public void testEmptyString() {
-            
-		assertEquals(0, Calculator.add(""));
-}
-        
-    @Test    
-     public void test1Valeure() {
-            
-		assertEquals(2, Calculator.add("2"));
-    
-}   
-        
-    @Test    
-        public void test2Valeursavecvirgule() {
-           
-		assertEquals(5, Calculator.add("2,3"));
-    
-}
-        @Test    
-        public void test3Valeursavecvirgule() {
-           
-		assertEquals(8, Calculator.add("2,3,3"));
-    
-}
-        
-      @Test
-    public void test1Ligne(){
-        
-    	assertEquals(6, Calculator.add("1\n2,3"));
-        
-    }
-  
-      @Test
-    public void testPlusGrandMille(){
-      
-    	assertEquals(50, Calculator.add("1000,50"));
-}
-    
-     @Test
-    public void testOtherDelimiter(){
-        
-    	assertEquals(3, Calculator.add("//;\n1;2"));
-}
-    
-    
-      @Test
-    public void testNegatif(){
-    	try {
-			Calculator.add("-1,2");
-		}
-		catch (IllegalArgumentException e){
-			assertEquals(e.getMessage(), "Les négatifs ne sont pas acceptés: -1");
-		}
 
-		try {
-			Calculator.add("2,-4,3,-5");
-		}
-		catch (IllegalArgumentException e){
-			assertEquals(e.getMessage(), "Les négatifs ne sont pas acceptés: -4,-5");
-		}
-}
-     public void testmultipleDelimiteur(){
-        
-    	assertEquals(6, Calculator.add("//[*]\\n1**2**3"));
-}
-  
+  @Before
+  public void setup() {
+
+  }
+
+  StringCalculator Calculator = new StringCalculator();
+
+  @Test
+  public void testEmptyString() {
+
+    assertEquals(0, Calculator.add(""));
+  }
+
+  @Test
+  public void test1Valeure() {
+
+    assertEquals(2, Calculator.add("2"));
+
+  }
+
+  @Test
+  public void test2Valeursavecvirgule() {
+
+    assertEquals(5, Calculator.add("2,3"));
+
+  }
+
+  @Test
+  public void test3Valeursavecvirgule() {
+
+    assertEquals(8, Calculator.add("2,3,3"));
+
+  }
+
+  @Test
+  public void test1Ligne() {
+
+    assertEquals(6, Calculator.add("1\n2,3"));
+
+  }
+
+  @Test
+  public void testPlusGrandMille() {
+
+    assertEquals(50, Calculator.add("1001,50"));
+  }
+
+  @Test
+  public void testOtherDelimiter() {
+
+    assertEquals(3, Calculator.add("//;\n1;2"));
+  }
+
+
+  @Test
+  public void testNegatif() {
+    try {
+      Calculator.add("-1,2");
+    } catch (IllegalArgumentException e) {
+      assertEquals(e.getMessage(), "Les nï¿½gatifs ne sont pas acceptï¿½s: -1");
+    }
+
+    try {
+      Calculator.add("2,-4,3,-5");
+    } catch (IllegalArgumentException e) {
+      assertEquals(e.getMessage(), "Les nï¿½gatifs ne sont pas acceptï¿½s: -4,-5");
+    }
+  }
+
+  @Test
+  public void testmultipleDelimiteur() {
+
+    assertEquals(6, Calculator.add("//[*][%]\\n1*2%3"));
+  }
+
 }
